5bb4d5a07232da693e7c7c2a0d9cc9b3ddbce8f9
aounhe <aounhe@hotmail.com>
2024-04-09 10:43:22 +0000

    connect flask to mangodb

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+@app.route("/")
+@app.route("/get_films")
+def get_films():
+   films = mongo.db.films.find()
+   return render_template("films.html", films=films)
